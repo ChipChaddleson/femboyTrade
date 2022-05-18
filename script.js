e42f7408e62a3fff7331d9e62fe3c8cb3cfbe2a8
ChipChaddleson <rich.a.denton@gmail.com>
2022-05-17 23:51:17 -0400

    added some more transition pags now they look even cooler

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
+    var comtd = 0;
     $('.titleTextMain').hide();
-    $(".top").slideUp(-4);
+    $(".top").slideUp(0);
+    $(".behind").hide();
+    $(".behind2").hide();
+    $(".behind3").hide();
     $(".titleTextMain").click(function (e) { 
-        e.preventDefault();
-        $(".top").slideDown(2000);
-        setTimeout(() => {
+        if (comtd == 3) {
+            $(".behind3").show();
+            // make behind2 explode 
+            $(".behind2").toggle("fold", {horizFirst: true, size: 500}, 1000);
+            comtd = 4;
             
-            $(".titleTextMain").css("color", "#9b379f");
-            $(".glow").css("text-shadow", " #56005a 10px 20px 45px");
-        }, 1000);
+        };
+        
+        if (comtd == 2) {
+            $(".behind2").show(0);
+            $(".behind").animate({width: 'toggle'}, 500);
+            comtd = 3;
+            
+        };
+        
+        
+        if (comtd == 1) {
+            $(".top").slideToggle(500);
+            comtd = 2;
+        };
+
+        if (comtd == 0) {
+            e.preventDefault();
+            $(".top").slideToggle(500);
+            setTimeout(() => {
+                
+                $(".titleTextMain").css("color", "#9b379f");
+                $(".letter").removeClass("glow");
+            }, 250);
+            setTimeout(() => {
+                $(".behind").show();
+            }, 500);
+            comtd = 1;
+        };
     });
 
     $('.titleTextSub').click(function () {
@@ -17,11 +48,8 @@ $(document).ready(function () {
 
     });
 
-    $('.letter').hover(function () {
-        $(this).addClass('glow');},
-        function () {$(this).removeClass('glow')
 
-    });
+
 
     $(".titleTextSub").hover(function () {
         $(this).text("the cuter they are!");},
